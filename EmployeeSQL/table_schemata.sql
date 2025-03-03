45829d9fb483358650f0fd146d25eb02bda6de88
Anqa Javed <anqajaved@gmail.com>
2025-03-03 15:25:31 -0500

    Final update: Fixed composite keys, column naming, and VARCHAR length

--- EmployeeSQL/table_schemata.sql
+++ EmployeeSQL/table_schemata.sql
@@ -4,16 +4,19 @@ CREATE TABLE departments (
 );
 CREATE TABLE dept_emp (
     emp_no INT NOT NULL,
-    dept_no VARCHAR(10) NOT NULL
+    dept_no VARCHAR(10) NOT NULL,
+    PRIMARY KEY (emp_no, dept_no)
 );
+
 CREATE TABLE dept_manager (
     dept_no VARCHAR(10) NOT NULL,
-    emp_no INT NOT NULL
+    emp_no INT NOT NULL,
+    PRIMARY KEY (dept_no, emp_no)
 );
 CREATE TABLE employees (
     emp_no INT PRIMARY KEY,
-    emp_title VARCHAR(10) NOT NULL,  
-    birth_date DATE NOT NULL,
+   title_id VARCHAR(10) NOT NULL,
+   birth_date DATE NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
     sex VARCHAR(1) NOT NULL,
@@ -34,10 +37,8 @@ ALTER TABLE dept_manager
 ADD CONSTRAINT fk_dept_manager_dept FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
 ADD CONSTRAINT fk_dept_manager_emp FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
 ALTER TABLE employees
-ADD CONSTRAINT fk_employees_title FOREIGN KEY (emp_title) REFERENCES titles(title_id);
+ADD CONSTRAINT fk_employees_title FOREIGN KEY (title_id) REFERENCES titles(title_id);
 ALTER TABLE salaries
 ADD CONSTRAINT fk_salaries_emp FOREIGN KEY (emp_no) REFERENCES employees(emp_no);
-SELECT conname AS constraint_name, conrelid::regclass AS table_name, pg_get_constraintdef(oid) 
-FROM pg_constraint
-WHERE contype = 'f';
+
 
